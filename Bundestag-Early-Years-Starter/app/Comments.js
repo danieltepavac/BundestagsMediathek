f675ae6a92ae44f882aa3d65aead6bbe9ee84c39
LisaAust <lmtsua@gmail.com>
2022-09-22 11:01:04 +0200

    für kommentare später

---
+++ Bundestag-Early-Years-Starter/app/Comments.js
@@ -0,0 +1,10 @@
+//Nutzerinteraktionen mit der Detailansicht speichern
+/*class Comments {
+    commentText;
+    timestamp;
+    label;
+
+    function updateCommentsDatabase(commentText) {
+        
+    }
+}
