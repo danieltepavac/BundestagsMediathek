fc7f7ebd3622407bd717165248b6ad05c2c74e09
LisaAust <lmtsua@gmail.com>
2022-09-22 11:48:03 +0200

    updated overview js

---
+++ Bundestag-Early-Years-Starter/app/resources/js/overview.js
@@ -0,0 +1,85 @@
+import DebugWatcher from "./utils/DebugWatcher.js";
+import RemoteSQLiteDatabase from "./database/RemoteSQLiteDatabase.js";
+
+function init() {
+    console.log("### Starting WebTech-App ###"); // eslint-disable-line no-console
+    DebugWatcher.connect(); // Enables live reloading when client code has changed
+    
+      initDB();
+      showSpeechesOverview();
+      myFunction();
+
+      document.getElementById ("searchButton").addEventListener ("click", search);
+      
+}
+
+async function initDB(){
+
+    let query, response, db;
+
+    // Create remote database connection
+    db = new RemoteSQLiteDatabase();
+    db.connect();
+}
+
+async function showSpeechesOverview(filterQuery = null){
+    let query, response, db,
+     speechesHtml = "";
+
+    db = new RemoteSQLiteDatabase();
+    db.connect();
+
+    query = `SELECT *
+            FROM Speech AS s
+            LEFT JOIN Agenda AS a
+                ON s.agendaId = a.agendaId`;
+    if (filterQuery && filterQuery.length > 0) {
+    query += ` WHERE s.date LIKE '%${filterQuery}%' OR
+                    s.speaker LIKE '%${filterQuery}%' OR
+                    a.title LIKE '%${filterQuery}%'`;    
+    }
+    
+    response = await db.runQuery(query);
+    console.log("######################");
+    console.log(response);
+    response.resultSet.forEach(speech => {
+        console.log("");
+        console.log("single speech");
+        console.log(speech);
+        const date = new Date(speech.date),
+         year = date.getFullYear();
+        speechesHtml +=`    
+        <div class="overview-speech-entry">
+            <p class="id">${speech.speechId}</p>
+            <p class="titel">${speech.title}</p>
+            <p class="speaker">${speech.speaker}</p>
+            <p class="year">${year}</p>
+            <a href="detail.html?speechId=${speech.speechId}">Zur Detailansicht</a>
+        </div>
+    `;
+    });
+    document.getElementById("overview-speeches-list").innerHTML = speechesHtml;
+}
+/** die Funktion zum Filtern brauchen wir wahrscheinlich doch nicht
+async function myFunction() {
+    var input, filter, list, entry, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    list = document.getElementById("overview-speeches-list");
+    entry = document.getElementsByClassName("overview-speech-entry");
+    for (i = 0; i < list.length; i++) {
+        a = list[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            list[i].style.display = "";
+        } else {
+            list[i].style.display = "none";
+        }
+    }
+}
+*/
+function search() {
+    const filterQuery = document.getElementById("searchInput").value;
+    showSpeechesOverview(filterQuery);    
+}
+init();
